656d5a1e77f884f7951e7865f245c2730e9e2394
Eder Fonseca <ederfonseca@outlook.com>
2020-06-02 10:03:52 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ContatosListagemComponent } from './contatos-listagem/contatos-listagem.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContatosListagemComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
